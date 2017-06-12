e0858740463b7460a6ea799810d4e8f4943b42e5
Jan Høydahl <jan.git@cominvent.com>
2017-06-12 12:19:56 +0200

    Added test for month range with country filter

--- valutareg/track.py
+++ valutareg/track.py
@@ -11,6 +11,8 @@ class QueryParamSource:
             self.terms = [line.strip() for line in ins.readlines()]
         with open(os.path.join(cwd, "currencycodes.txt"), "r") as ins:
             self.currency = [line.strip() for line in ins.readlines()]
+        with open(os.path.join(cwd, "countrycodes.txt"), "r") as ins:
+            self.countrycode = [line.strip() for line in ins.readlines()]
 
     def partition(self, partition_index, total_partitions):
         return self
@@ -98,8 +100,46 @@ class CurrencyCode10kQueryParamSource(QueryParamSource):
         }
         return result
 
+class DateRangeMonthPerCountryLastnameAggQueryParamSource(QueryParamSource):
+    def params(self):
+        month = random.randint(12, 360)
+        result = {
+            "body": {
+                "size": 10000,
+                "query": {
+                    "bool": {
+                        "filter": [
+                            {
+                                "range" : {
+                                    "transdato" : {
+                                        "gte" : "now-%sM/M" % month,
+                                        "lt" : "now-%sM/M" % (month-1)
+                                    }
+                                }
+                            },
+                            {
+                                "term": {
+                                    "utlland": "%s" % random.choice(self.countrycode)
+                                }
+                            }
+                        ]
+                    }
+                },
+                "aggs" : {
+                    "lastnames" : {
+                        "terms" : { "field" : "noretternavn.keyword" }
+                    }
+                }
+            },
+            "index": None,
+            "type": None,
+            "use_request_cache": self._params.get("use_request_cache", False)
+        }
+        return result
+
 def register(registry):
     registry.register_param_source("currency-code-query-source", CurrencyCodeQueryParamSource)
     registry.register_param_source("currency-code-10-query-source", CurrencyCode10QueryParamSource)
     registry.register_param_source("currency-code-10k-query-source", CurrencyCode10kQueryParamSource)
     registry.register_param_source("foreign-last-query-source", ForeignLastnameQueryParamSource)
+    registry.register_param_source("daterange-country-lnagg-query-source", DateRangeMonthPerCountryLastnameAggQueryParamSource)
